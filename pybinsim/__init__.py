131803d743f6812a9caa14db17fe7aad98cb95c3
Thomas Köllmer <thomas.koellmer@tu-ilmenau.de>
2017-12-02 14:15:44 +0100

    - moved logging initialization to __init__

--- pybinsim/__init__.py
+++ pybinsim/__init__.py
@@ -1,4 +1,24 @@
+import logging
+
+
 def __version__():
     return "1.0.1"
 
 from pybinsim.application import BinSim
+
+
+def init_logging(loglevel):
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(loglevel)
+
+    formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger("pybinsim")
+    logger.addHandler(console_handler)
+    logger.setLevel(loglevel)
+
+    return logger
+
+
+logger = init_logging(logging.DEBUG)
