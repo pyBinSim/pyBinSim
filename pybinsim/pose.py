1e7155e267e83bd6ade6bcadd516684b495ceaa4
Thomas Köllmer <thomas.koellmer@tu-ilmenau.de>
2018-01-21 19:06:38 +0100

    Make Pose backwards compatible to 6-value version

--- pybinsim/pose.py
+++ pybinsim/pose.py
@@ -1,4 +1,7 @@
+import logging
 from collections import namedtuple
+
+logger = logging.getLogger("pybinsim.Pose")
 
 
 class Orientation(namedtuple('Orientation', ['yaw', 'pitch', 'roll'])):
@@ -8,12 +11,13 @@ class Orientation(namedtuple('Orientation', ['yaw', 'pitch', 'roll'])):
 class Position(namedtuple('Position', ['x', 'y', 'z'])):
     pass
 
+
 class Custom(namedtuple('CustomValues', ['a', 'b', 'c'])):
     pass
 
 
 class Pose:
-    def __init__(self, orientation, position, custom):
+    def __init__(self, orientation, position, custom=Custom(0, 0, 0)):
         self.orientation = orientation
         self.position = position
         self.custom = custom
@@ -25,12 +29,20 @@ class Pose:
 
     @staticmethod
     def from_filterValueList(filter_value_list):
-        if len(filter_value_list) != 9:
-            print(filter_value_list)
-            assert False
 
-        orientation = Orientation(filter_value_list[0], filter_value_list[1], filter_value_list[2])
-        position = Position(filter_value_list[3], filter_value_list[4], filter_value_list[5])
-        custom = Custom(filter_value_list[6], filter_value_list[7], filter_value_list[8])
+        # 'old' format: orientation - position
+        if len(filter_value_list) == 6:
+            orientation = Orientation(filter_value_list[0], filter_value_list[1], filter_value_list[2])
+            position = Position(filter_value_list[3], filter_value_list[4], filter_value_list[5])
 
-        return Pose(orientation, position, custom)
+            return Pose(orientation, position)
+
+        # 'new' format: orientation - position - custom
+        if len(filter_value_list) == 9:
+            orientation = Orientation(filter_value_list[0], filter_value_list[1], filter_value_list[2])
+            position = Position(filter_value_list[3], filter_value_list[4], filter_value_list[5])
+            custom = Custom(filter_value_list[6], filter_value_list[7], filter_value_list[8])
+
+            return Pose(orientation, position, custom)
+
+        raise RuntimeError("Unable to parse filter list: {}".format(filter_value_list))
--- tests/test_pose.py
+++ tests/test_pose.py
@@ -13,10 +13,21 @@ class TestPose(TestCase):
         self.assertEqual(orientation.pitch, '20')
         self.assertEqual(position.z, '3')
 
-        self.assertEqual(pose.create_key(), "10,20,30,1,2,3")
+        self.assertEqual(pose.create_key(), "10,20,30,1,2,3,0,0,0")
 
-    def test_from_filter_value_list(self):
+    def test_from_filter_value_list_6(self):
 
         pose = Pose.from_filterValueList([10, 20, 30, 1, 2, 3])
         self.assertTrue(pose.orientation.yaw, 10)
         self.assertTrue(pose.position.x, 1)
+
+    def test_from_filter_value_list_9(self):
+
+        pose = Pose.from_filterValueList([10, 20, 30, 1, 2, 3, 11, 22, 33])
+        self.assertTrue(pose.orientation.yaw, 10)
+        self.assertTrue(pose.position.x, 1)
+        self.assertTrue(pose.custom.b, 22)
+
+    def test_from_filter_value_invalid(self):
+        with self.assertRaises(RuntimeError):
+            Pose.from_filterValueList([1, 2, 3])
